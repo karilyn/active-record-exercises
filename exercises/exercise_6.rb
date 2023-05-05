a611e8a44dba7bd523ab777a7c8357657114b485
Karilyn Kempton <karilyn.kempton@gmail.com>
2023-05-05 12:06:52 -0700

    Work through CRUD operations as practice

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,10 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "John", last_name: "Doe", hourly_rate: 40)
+@store1.employees.create(first_name: "Jane", last_name: "Doe", hourly_rate: 40)
+@store2.employees.create(first_name: "Karilyn", last_name: "Kempton", hourly_rate: 50)
+@store2.employees.create(first_name: "Kathleen", last_name: "Smith", hourly_rate: 50)
+@store2.employees.create(first_name: "Josephine", last_name: "Stephenson", hourly_rate: 40)
+
