aeac01d549cc962f6ddb3845589cbcf97e29132a
Karilyn Kempton <karilyn.kempton@gmail.com>
2023-05-05 12:05:21 -0700

    Add validations

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,13 @@
 class Store < ActiveRecord::Base
+  has_many :employees
+  validates :name, length: { minimum: 3 }, presence: true
+  validates :annual_revenue, numericality: { greater_than_or_equal_to: 0}, presence: true
+  validate :must_carry_apparel
 
+  def must_carry_apparel
+    if !mens_apparel && !womens_apparel
+      errors.add(:mens_apparel, "must carry at least one type of apparel")
+      errors.add(:womens_apparel, "must carry at least one type of apparel")
+    end
+  end
 end
